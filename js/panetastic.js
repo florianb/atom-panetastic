365c63776041f8e26955ef4f7f725ff894da3491
Florian Breisch <florian.breisch@mindkeeper.de>
2015-08-24 10:30:42 +0200

    fixed dependency for views
    Due to the 1.0-release of Atom, the views were moved from the core to
    the atom-space-en-views-package.

--- js/panetastic.js
+++ js/panetastic.js
@@ -4,7 +4,7 @@
     __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
-  _ref = require('atom'), $ = _ref.$, View = _ref.View;
+  _ref = require('atom-space-pen-views'), View = _ref.View, $ = _ref.$;
 
   Panetastic = (function(_super) {
     __extends(Panetastic, _super);
